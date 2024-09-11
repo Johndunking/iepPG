b56f40f6f01c1bb8550acc02ca3157279824e1e2
John Duncan <johnduncan@Johns-MacBook-Pro.local>
2024-09-10 20:39:47 -0700

    debug

--- server/server.js
+++ server/server.js
@@ -107,11 +107,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-
 function processData(text) {
   const data = {};
 
